a360a6d9508b5e331149c31335162f3f6b3d3cbb
Allana Evellyn <allanaevellynm@gmail.com>
2023-09-15 20:55:54 -0300

    feat: created message class

--- backend/models/message.js
+++ backend/models/message.js
@@ -3,8 +3,20 @@ class Message {
         this.sender = sender;
         this.receiver = receiver;
         this.text = text;
-        const options = { timeZone: 'America/Sao_Paulo' }; 
-        this.timestamp = new Date().toLocaleString('pt-BR', options);
+        this.timestamp = this.getCurrentTimestamp();
+        this.maxTextLength = 1000; 
+        this.validateTextLength();
+    }
+
+    getCurrentTimestamp() {
+        const options = { timeZone: 'America/Sao_Paulo' };
+        return new Date().toLocaleString('pt-BR', options);
+    }
+
+    validateTextLength() {
+        if (this.text.length > this.maxTextLength) {
+            throw new Error("O texto da mensagem excede o limite de caracteres permitido.");
+        }
     }
 }
 
